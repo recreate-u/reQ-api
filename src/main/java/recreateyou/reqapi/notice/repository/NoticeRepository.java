b8cb88cdb9ef87c13a2af7f87c81b0a0ca0386de
정희제 <jungheejae931012@gmail.com>
2024-01-05 11:33:56 +0900

    notice, report의 repository, service, vo 파일생성

--- src/main/java/recreateyou/reqapi/notice/repository/NoticeRepository.java
+++ src/main/java/recreateyou/reqapi/notice/repository/NoticeRepository.java
@@ -1,9 +1,4 @@
 package recreateyou.reqapi.notice.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import recreateyou.reqapi.notice.entity.NoticeEntity;
-
-@Repository
-public interface NoticeRepository extends JpaRepository<NoticeEntity, Long> {
+public interface NoticeRepository {
 }
