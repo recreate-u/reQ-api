2ea855c505f199d09269c32f0f1cafc523d828b2
정희제 <jungheejae931012@gmail.com>
2024-01-12 17:27:23 +0900

    어노테이션 추가

--- src/main/java/recreateyou/reqapi/notice/service/NoticeService.java
+++ src/main/java/recreateyou/reqapi/notice/service/NoticeService.java
@@ -1,4 +1,7 @@
 package recreateyou.reqapi.notice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NoticeService {
 }
--- src/main/java/recreateyou/reqapi/report/controller/ReportController.java
+++ src/main/java/recreateyou/reqapi/report/controller/ReportController.java
@@ -1,4 +1,7 @@
 package recreateyou.reqapi.report.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReportController {
 }
