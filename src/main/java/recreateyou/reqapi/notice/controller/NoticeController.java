0d026b99d84c28a8b04ad3fc248629fea212a8e0
정희제 <jungheejae931012@gmail.com>
2024-01-12 17:17:21 +0900

    NoticeController, ReportController 추가

--- src/main/java/recreateyou/reqapi/notice/controller/NoticeController.java
+++ src/main/java/recreateyou/reqapi/notice/controller/NoticeController.java
@@ -1,47 +1,4 @@
 package recreateyou.reqapi.notice.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import recreateyou.reqapi.notice.entity.NoticeEntity;
-import recreateyou.reqapi.notice.service.NoticeService;
-import recreateyou.reqapi.notice.vo.NoticeRequestVO;
-import recreateyou.reqapi.notice.vo.NoticeResponseVO;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/notice")
 public class NoticeController {
-
-    private final NoticeService noticeService;
-
-    @PostMapping("/{notice-seq}")
-    public void registerNotice(@PathVariable("notice-seq") Long noticeSeq, @RequestBody NoticeRequestVO noticeRequestVO){
-        noticeService.registerNotice(noticeSeq, noticeRequestVO);
-    }
-
-    // 공지 개별로 확인
-    @GetMapping("/{notice-seq}")
-    public NoticeResponseVO getNotice(@PathVariable("notice-seq") Long noticeSeq){
-        return noticeService.getNotice(noticeSeq);
-    }
-
-    // 전체 공지
-    @GetMapping
-    public List<NoticeResponseVO> getAllNotice(){
-        return noticeService.getAllNotice();
-    }
-
-    @PatchMapping("/{notice-seq}")
-    public void patchNotice(@PathVariable("notice-seq") Long noticeSeq, @RequestBody NoticeRequestVO noticeRequestVO){
-        noticeService.patchNotice(noticeSeq, noticeRequestVO);
-    }
-
-    @DeleteMapping("/{notice-seq}")
-    public void deleteNotice(@PathVariable("notice-seq") Long noticeSeq){
-        noticeService.deleteNotice(noticeSeq);
-    }
 }
