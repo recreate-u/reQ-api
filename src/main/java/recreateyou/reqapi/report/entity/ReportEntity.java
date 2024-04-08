2a13b11affa8057b9837d6084773e30f3bf7b020
정희제 <jungheejae931012@gmail.com>
2024-04-08 18:15:36 +0900

    공지사항 api 수정, 신고 api

--- src/main/java/recreateyou/reqapi/report/entity/ReportEntity.java
+++ src/main/java/recreateyou/reqapi/report/entity/ReportEntity.java
@@ -2,14 +2,15 @@ package recreateyou.reqapi.report.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import recreateyou.reqapi.feed.entity.FeedEntity;
+import recreateyou.reqapi.report.vo.ReportRequestVO;
+import recreateyou.reqapi.report.vo.ReportResponseVO;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Entity
 @Table(name = "REPORT")
@@ -25,8 +26,20 @@ public class ReportEntity {
 
     @JoinColumn(name = "FEED_SEQ")
     @ManyToOne
-    private FeedEntity feedSeq;
+    private @NonNull Long feedSeq;
 
     @OneToMany(mappedBy = "reportReasons")
-    List<ReportKeyEntity> ReportReasons;
+    @NonNull
+    private String ReportReasons;
+
+    public ReportEntity(Long reportSeq, ReportRequestVO reportRequestVO){
+        this.reportSeq = reportRequestVO.reportSeq();
+        this.context = reportRequestVO.context();
+        this.feedSeq = reportRequestVO.feedSeq();
+        this.ReportReasons = reportRequestVO.reportReason();
+    }
+
+    public ReportResponseVO toResponseVO(){
+        return new ReportResponseVO(this.getReportSeq(), this.getContext(), this.getFeedSeq(), this.getReportReasons());
+    }
 }
