67445e19ff79a9621aeca284b9601f4be419b8b9
박찬호 <tareun3406@gmail.com>
2024-07-15 23:51:15 +0900

    fix: local datasource 제거 및 테이블 create 시 기본 데이터 초기화

--- src/main/java/recreateyou/reqapi/auth/entity/AuthCompositeId.java
+++ src/main/java/recreateyou/reqapi/auth/entity/AuthCompositeId.java
@@ -3,10 +3,12 @@ package recreateyou.reqapi.auth.entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import recreateyou.reqapi.auth.enums.Role;
 
 import java.io.Serializable;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @SuppressWarnings("unused")
 public class AuthCompositeId implements Serializable {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into role_list values ('USER');
